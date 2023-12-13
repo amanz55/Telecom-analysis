a6d0dd362431d1303c19f67abae4f5319f2003b1
amanz55 <amanuelalemayehu449@gmail.com>
2023-12-13 22:52:22 +0300

    (Feat): Full analysis completed

---
+++ local_settings.py
@@ -0,0 +1,6 @@
+postgresql = {'pguser':'postgres',
+              'pgpasswd':'368545',
+              'pghost':'localhost',
+              'pgport': 5432,
+              'pgdb': 'testing_db'
+             }
