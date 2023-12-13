79e12873c890af820fc5d485767f723d0dff4859
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2023-12-13 22:54:21 +0300

    Delete local_settings.py

--- local_settings.py
+++
@@ -1,6 +0,0 @@
-postgresql = {'pguser':'postgres',
-              'pgpasswd':'368545',
-              'pghost':'localhost',
-              'pgport': 5432,
-              'pgdb': 'testing_db'
-             }
